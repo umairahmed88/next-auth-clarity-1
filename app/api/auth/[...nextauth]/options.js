c9ffb3cfebdbc7a878fc466f6e3d3cb23ae543c5
umairahmed88 <umairahmed_88@ymail.com>
2023-12-23 04:11:52 +0500

    added route and auth to the app

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,55 @@
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+	providers: [
+		GitHubProvider({
+			profile: {
+				profile(profile) {
+					console.log("GitHub Profile:".profile);
+
+					let userRole = "GitHub User";
+
+					if (profile?.email == "umairahmed_88@ymail.com") {
+						userRole = "admin";
+					}
+
+					return {
+						...profile,
+						role: userRole,
+					};
+				},
+			},
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET,
+		}),
+
+		GoogleProvider({
+			profile: {
+				profile(profile) {
+					console.log("Google Profile:".profile);
+
+					return {
+						...profile,
+						id: profile.sub,
+						role: userRole,
+					};
+				},
+			},
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+		}),
+	],
+
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) token.role = user.role;
+			return token;
+		},
+
+		async session({ session, token }) {
+			if (session?.user) session.user.role = token.role;
+			return session;
+		},
+	},
+};
